708c36571ad56399148bb671ff48500c72551f6d
Leo Stoll <guillermostoll61@gmail.com>
2024-04-23 22:20:25 +0200

    Añade la alerta en el HTML
    131. Añadiendo la alerta en el HTML

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
     
 
 
@@ -23,10 +24,13 @@ document.addEventListener('DOMContentLoaded', function(){
     } 
 
     function mostrarAlerta(){
+        //Generar alerta en HTML
     const error = document.createElement('P');
     error.textContent = 'Hubo un error...';
+    error.classList.add('bg-red-600', 'text-white','text-center');
     
-    console.log(error);
+    //inyectar el error al formulario
+    formulario.appendChild(error)
 
     }
 
