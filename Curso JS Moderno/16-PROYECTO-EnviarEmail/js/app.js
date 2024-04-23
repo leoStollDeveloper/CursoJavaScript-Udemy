0df187803ea21b7e8a94caec7d25b44a6ad43b6e
Leo Stoll <guillermostoll61@gmail.com>
2024-04-23 21:48:40 +0200

    creando Alerta de error
    130. Creando una alerta de error en la validación

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -16,11 +16,20 @@ document.addEventListener('DOMContentLoaded', function(){
  
     function validar(evento) {
         if (evento.target.value.trim() === '') {
-            console.log('Esta vacio')
+            mostrarAlerta();
         }else {
-            console.log('si hay algo...')
+            console.log('si hay algo...');
         }
     } 
+
+    function mostrarAlerta(){
+    const error = document.createElement('P');
+    error.textContent = 'Hubo un error...';
+    
+    console.log(error);
+
+    }
+
 
 
 
