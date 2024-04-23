c55c975ef33ef792bf49c47156b66e65d71416bc
Leo Stoll <guillermostoll61@gmail.com>
2024-04-23 13:42:27 +0200

    Primeros pasos
    Simulador de Envio de Emails (1/17)

---
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    //Seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+    
+
+
+
+    // console.log(inputMensaje);
+
+
+
+
+})
