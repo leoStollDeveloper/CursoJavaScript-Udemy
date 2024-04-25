e3c7d9a3c2792c051ac3a04a0d3934443a802cbf
Leo Stoll <guillermostoll61@gmail.com>
2024-04-25 11:30:42 +0200

    139. Resetear el Formulario

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -14,12 +14,26 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
     const btnSubmit = document.querySelector('#formulario button[type="submit"]')
+    const btnReset = document.querySelector('#formulario button[type="reset"]')
 
 
     //Asignar eventos
     inputEmail.addEventListener('input', validar);
     inputAsunto.addEventListener('input', validar);
     inputMensaje.addEventListener('input', validar);
+
+    btnReset.addEventListener('click', function(evento){
+        evento.preventDefault();
+
+        //Reiniciar el Objeto
+        email.email = '';
+        email.asunto = '';
+        email.mensaje = '';
+
+        formulario.reset();
+        comprobarEmail();
+
+    })
         
 
 
